8f2bae2dfae9d48a08dca04415db33ba953361eb
LUTZ <arthur.lutz8@etu.univ-lorraine.fr>
2025-03-07 09:20:25 +0100

    Changements input password - LUTZ

--- client/controleur/register.js
+++ client/controleur/register.js
@@ -160,3 +160,10 @@ document.querySelector('form').addEventListener('submit', (e) => {
     e.preventDefault();
     register();
 });
+
+document.getElementById("togglePassword").addEventListener("click", function () {
+    const passwordField = document.getElementById("mdp");
+    const type = passwordField.getAttribute("type") === "password" ? "text" : "password";
+    passwordField.setAttribute("type", type);
+    this.classList.toggle("fa-eye-slash");
+});
